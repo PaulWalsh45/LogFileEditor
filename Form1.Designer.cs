6bf75c33b617ca9f61e2e39cee3c3b7df36ac6ac
Paul Walsh <PXWalsh@spencerstuart.com>
2016-06-03 10:50:07 +0100

    First Commit, app working as required

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace LogFileEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnWriteFile = new System.Windows.Forms.Button();
+            this.btnReadFile = new System.Windows.Forms.Button();
+            this.txtWriteFilepath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtReadFilepath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAppendFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnWriteFile
+            // 
+            this.btnWriteFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWriteFile.Location = new System.Drawing.Point(41, 266);
+            this.btnWriteFile.Name = "btnWriteFile";
+            this.btnWriteFile.Size = new System.Drawing.Size(110, 32);
+            this.btnWriteFile.TabIndex = 7;
+            this.btnWriteFile.Text = "Write File";
+            this.btnWriteFile.UseVisualStyleBackColor = true;
+            this.btnWriteFile.Click += new System.EventHandler(this.btnWriteFile_Click);
+            // 
+            // btnReadFile
+            // 
+            this.btnReadFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReadFile.Location = new System.Drawing.Point(41, 105);
+            this.btnReadFile.Name = "btnReadFile";
+            this.btnReadFile.Size = new System.Drawing.Size(110, 32);
+            this.btnReadFile.TabIndex = 8;
+            this.btnReadFile.Text = "Read File";
+            this.btnReadFile.UseVisualStyleBackColor = true;
+            this.btnReadFile.Click += new System.EventHandler(this.btnReadFile_Click);
+            // 
+            // txtWriteFilepath
+            // 
+            this.txtWriteFilepath.Location = new System.Drawing.Point(199, 232);
+            this.txtWriteFilepath.Name = "txtWriteFilepath";
+            this.txtWriteFilepath.Size = new System.Drawing.Size(254, 20);
+            this.txtWriteFilepath.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(37, 232);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Writefile Filepath";
+            // 
+            // txtReadFilepath
+            // 
+            this.txtReadFilepath.Location = new System.Drawing.Point(199, 70);
+            this.txtReadFilepath.Name = "txtReadFilepath";
+            this.txtReadFilepath.Size = new System.Drawing.Size(254, 20);
+            this.txtReadFilepath.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(37, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Readfile Filepath";
+            // 
+            // btnAppendFile
+            // 
+            this.btnAppendFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAppendFile.Location = new System.Drawing.Point(199, 266);
+            this.btnAppendFile.Name = "btnAppendFile";
+            this.btnAppendFile.Size = new System.Drawing.Size(107, 32);
+            this.btnAppendFile.TabIndex = 9;
+            this.btnAppendFile.Text = "AppendFile";
+            this.btnAppendFile.UseVisualStyleBackColor = true;
+            this.btnAppendFile.Click += new System.EventHandler(this.btnAppendFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 369);
+            this.Controls.Add(this.btnAppendFile);
+            this.Controls.Add(this.btnWriteFile);
+            this.Controls.Add(this.btnReadFile);
+            this.Controls.Add(this.txtWriteFilepath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtReadFilepath);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnWriteFile;
+        private System.Windows.Forms.Button btnReadFile;
+        private System.Windows.Forms.TextBox txtWriteFilepath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtReadFilepath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAppendFile;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design.Serialization;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LogFileEditor
+{
+    public partial class Form1 : Form
+    {
+        List<string> ReadLines = new List<string>();
+        
+        
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            txtWriteFilepath.Text = "LogFilesAll_Id.txt";//Loads the filepath to the write file text box, if you wish to write to same file all the time
+        }
+
+        private void btnReadFile_Click(object sender, EventArgs e)
+        {
+
+            var filepath = txtReadFilepath.Text;
+            StreamReader reader = new StreamReader(filepath);
+            string line = null;
+            int count = 0;
+
+            //<-- comment one of the following 2 lines depending if you want to filter the Read or Not -->
+            //count = ReadTextFileWithFilter(reader, count);
+            count = ReadTextFile(reader, count);
+
+            reader.Close();
+            MessageBox.Show("Lines in File: " + count.ToString());
+        }
+
+        private int ReadTextFile(StreamReader reader, int count)
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                ReadLines.Add(line); 
+                count++;
+            }
+            return count;
+        }
+
+        private int ReadTextFileWithFilter(StreamReader reader, int count)
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (line.Contains("Quest.Internal/People/Profile/"))
+                {
+                    ReadLines.Add(line);
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        private void btnWriteFile_Click(object sender, EventArgs e)
+        {
+            
+            
+            var filepath = txtWriteFilepath.Text;
+
+            StreamWriter writer = new StreamWriter(filepath);
+
+            foreach (var str in ReadLines)
+            {
+                writer.WriteLine(ExtractNumber(str));
+                //writer.WriteLine(str);
+            }
+            writer.Close();
+            MessageBox.Show("Success");
+        }
+
+        public string ExtractNumber(string line)
+        {
+            List<string> WriteLines = new List<string>(); 
+            string personId = null;
+            string[] lineContent = line.Split(' ');
+            char[] charsToTrim = { '/' };
+
+            foreach (var element in lineContent)
+            {
+                
+                if (element.Contains("Quest.Internal/People/Profile/"))
+                {
+                    int idLength = element.Length;
+                    int startOfId = 30;
+                    int endOfId = idLength - startOfId;
+                    int duplicatesFound = 0;
+                    
+                    
+                    personId = element.Substring(startOfId, endOfId);
+                    personId=personId.Trim(charsToTrim);//trim leading slashes
+
+                    //trim from trailing slash onwards
+                    int indexOfTrailingSlash = personId.IndexOf("/");
+                    if (indexOfTrailingSlash>0)
+                    {
+                        personId=personId.Substring(0,indexOfTrailingSlash);
+                        
+                        //check for duplicate Id's and return empty string if Id already exists
+                        if (!(WriteLines.Contains(personId)))
+                        {
+                            WriteLines.Add(personId); 
+                            
+                        }
+                        else
+                        {
+                            MessageBox.Show("duplicates found: " + personId);
+                            personId = String.Empty;
+                            duplicatesFound++;
+                            
+                        }
+                        
+
+                    }
+
+                }
+            }
+            return personId;
+        }
+
+        private void btnAppendFile_Click(object sender, EventArgs e)
+        {
+            
+            var filepath = txtWriteFilepath.Text;
+
+            StreamWriter writer = new StreamWriter(filepath);
+
+            foreach (var str in ReadLines)
+            {
+                writer.Write(str+",");
+            }
+            writer.Close();
+            MessageBox.Show("Success");
+        }
+    }
+}
